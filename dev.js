0c6c6952c8b21bf9b0705ceaf2816dc1573ab65b
John O'Malley <john.c.omalley@gmail.com>
2017-01-31 10:47:49 -0600

    add hot reload in dev mode

--- dev.js
+++ dev.js
@@ -1,20 +1,43 @@
 const express = require('express')
 const devMiddleware = require('webpack-dev-middleware')
+const hotMiddleware = require('webpack-hot-middleware')
 const lessMiddleware = require('less-middleware')
 const webpack = require('webpack')
-const webpackDevConfig = require('./webpack.dev.config')
 
-const middlewareOptions = {
-  stats: {
-    colors: true
+const config = Object.assign({}, require('./webpack.config.js'), {
+  entry: [
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
+    'babel-polyfill',
+    './src/index.js'
+  ],
+  output: {
+    path: '/',
+    filename: 'bundle.js'
   },
-  noInfo: true
-}
+  devtool: 'cheap-inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"'
+      }
+    })
+  ]
+})
+
+const compiler = webpack(config)
 
 console.log('DEV mode')
 
 module.exports = app => {
   app.use('/images', express.static('images'))
-  app.use(devMiddleware(webpack(webpackDevConfig), middlewareOptions))
+  app.use(devMiddleware(compiler, {
+    stats: {
+      colors: true
+    },
+    noInfo: true
+  }))
+  app.use(hotMiddleware(compiler))
   app.use(lessMiddleware('css', {dest: 'public'}))
 }
